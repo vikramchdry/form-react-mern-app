63c1b8235ffee6474da061a4ac976dc762a09ddd
Karol <karol.jedryka@gmail.com>
2021-04-16 12:08:57 +0200

    proxy for email service

---
+++ client/config-overrides.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // Extend/override the dev server configuration used by CRA
+    // See: https://github.com/timarney/react-app-rewired#extended-configuration-options
+    devServer: function (configFunction) {
+        console.log("dev server overrides");
+        return function (proxy, allowedHost) {
+            // Create the default config by calling configFunction with the proxy/allowedHost parameters
+            // Default config: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/webpackDevServer.config.js
+            
+            const config = configFunction(proxy, allowedHost);
+
+            config.proxy = {
+                '/api': {
+                    target: 'https://extensi.io',
+                    changeOrigin: true,
+                }
+            }
+
+            return config;
+        };
+    },
+};
